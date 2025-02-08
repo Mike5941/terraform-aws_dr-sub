2bf6dc4855064ff1f84e92c5a2d36e51c41d55f3
Mike5941 <mike9159@naver.com>
2025-02-08 16:58:28 +0900

    Edit

--- prod/network/primary/output.tf
+++ prod/network/primary/output.tf
@@ -13,24 +13,11 @@ output "private_subnets" {
   value       = module.primary.private_subnets
 }
 
-output "database_subnets" {
-  description = "Database Subnet ID"
-  value       = module.primary.database_subnets
-}
-
 output "azs" {
   value = module.primary.azs
 }
 
-output "db_subnet_group" {
-  description = "RDS Subnet Group Name"
-  value       = module.primary.db_subnet_groups
-}
 
-output "cache_subnet_group" {
-  description = "Cache Subnet Group Name"
-  value       = module.primary.cache_subnet_groups
-}
 
 #output "vpn_endpoint_id" {
 #  value = aws_ec2_client_vpn_endpoint.vpn.id
